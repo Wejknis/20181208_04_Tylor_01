90a928dfbd3d08ee4350d5e428feb38c41bd5f31
Wejknis <weja077@gmail.com>
2018-12-08 11:41:35 +0100

    1 2 3  start działa

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int liczba_slow, char * tablica_slow[]) {
+
+    int dokladnosc = 50;
+    double e = 1.0;
+    int index;
+    double skladnik = 1.0;
+
+    double x = atof(tablica_slow[1]);
+
+    for (index = 1; index <= dokladnosc ; ++index) {
+     skladnik *= x / index;
+        e += skladnik;
+    }
+printf("e = %lf", e);
+
     return 0;
 }
